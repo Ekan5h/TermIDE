1ac6beea9018bea76aa175df623a67f5eaf372ca
Ekansh Mahendru <ekanshmahendru@gmail.com>
2020-05-06 17:44:57 +0530

    Adds editor functionality
    TODO
    * Add curup() and curdown()
    * Add page scrolling on overflow

--- src/elements.h
+++ src/elements.h
@@ -4,8 +4,8 @@ using namespace std;
 
 class ioFrame{
     public:
-    int startx, starty, height, width, cursor, pointer, topline, screenlines;
-    string content, screen;
+    int startx, starty, height, width, cursor, pointer, topline;
+    string content;
     WINDOW * win;
     /*
     (startx, starty) : start location on sceen
@@ -13,7 +13,6 @@ class ioFrame{
     cursor           : location of cursor on the screen
     pointer          : location of cursor in screen string
     topline          : the character at which the printing of page starts
-    screenlines      : how many lines are printed on the screen
     content          : content in the file
     screen           : part of content displayed on the screen
     */
@@ -25,12 +24,12 @@ class ioFrame{
         width = w;
         content = "";
         pointer = 0;
-        screenlines = 1;
         win = newwin(height, width, starty, startx);
         box(win, 0, 0);
         wattron(win, A_BOLD | COLOR_PAIR(2));
         mvwaddstr(win, 0, 2, s.c_str());
         wattroff(win, A_BOLD | COLOR_PAIR(2));
+        setcursor();
         wrefresh(win);
     }
 
@@ -50,6 +49,7 @@ class ioFrame{
     pair<int, int> getcursor(){
         int r = 1 + cursor/(width-2);
         int c = 1 + cursor%(width-2);
+        wmove(win, r, c);
         return {startx+c, starty+r};
     }
 
@@ -62,7 +62,7 @@ class ioFrame{
         int charinline = 0;
         int lines = 0;
         string output = "";
-        for(int i = topline; i<content.length(); i++){
+        for(int i = topline; i<content.length(); i++){  
             char c = content[i];
             if(c == '\n'){
                 lines++;
@@ -80,9 +80,103 @@ class ioFrame{
         return output;
     }
 
-    void typechr(char c){
-        
+    void cleartillend(){
+        int temp = cursor;
+        while(cursor < (height-2)*(width-2)){
+            waddch(win, ' ');
+            cursor++;
+            setcursor();
+        }
+        cursor = temp;
+        focus();
+        wrefresh(win);
     }
+
+    void typechr(char c){
+        content = content.substr(0,pointer) + c + content.substr(pointer,content.length());
+        cleartillend();
+        putstr(content.substr(pointer,content.length()));
+        pointer++;
+        if(c != '\n')
+            cursor++;
+        else{
+            cursor+= width-2 - cursor%(width-2);
+        }
+        focus();
+    }
+
+    void untypechr(){
+        if(pointer==0) return ;
+        char delchar = content[pointer-1];
+        content = content.substr(0,pointer-1)+content.substr(pointer,content.length());
+        pointer--;
+        int temp = pointer;
+        if(delchar == '\n'){
+            cursor -= width-2 + cursor%(width-2);
+            while(pointer>=0 && content[--pointer]!='\n');
+            while(++pointer!=temp) cursor++;
+        }else
+            cursor--;
+        setcursor();
+        cleartillend();
+        putstr(content.substr(pointer,content.length()));
+        focus();
+    }
+
+    void putstr(string s){
+        int temp = cursor;
+        for(char c:s){
+            if(c!='\n'){
+                waddch(win, c);
+                cursor++;
+            }else{
+                cursor+= width-2 - cursor%(width-2);
+            }
+            if(cursor >= (height-2)*(width-2)) break;
+            setcursor();
+        }
+        cursor = temp;
+        focus();
+        wrefresh(win);
+    }
+
+    void curdown(){
+
+    }
+
+    void curup(){
+    }
+
+    void curleft(){
+        if(pointer == 0) return ;
+        pointer-=1;
+        if(content[pointer] == '\n'){
+            cursor -= width-2 + cursor%(width-2);
+            while(pointer>=0 && content[--pointer]!='\n');
+            int temp = pointer;
+            while(content[++pointer]!='\n'){
+                if(pointer - temp == width-2){
+                    cursor -= width-2;
+                }
+                cursor++;
+            }
+        }else{
+            cursor-=1;
+        }
+        focus();
+    }
+
+    void curright(){
+        if(pointer == content.length()) return ;
+        if(content[pointer] == '\n'){
+            cursor += width-2 - cursor%(width-2);
+        }else{
+            cursor+=1;
+        }
+        pointer+=1;
+        focus();
+    }
+
 };
 
 class toolbar{
--- src/main.cpp
+++ src/main.cpp
@@ -18,17 +18,43 @@ int main(){
     int ROW, COL;
     getmaxyx(stdscr, ROW, COL);
     ROW--;
-    ioFrame * code = new ioFrame(0, 0, ROW, (2*COL)/3, "Editor");
-    ioFrame * arg = new ioFrame((2*COL)/3, 0, ROW/4, COL-(2*COL)/3, "Arguments");
-    ioFrame * input = new ioFrame((2*COL)/3, ROW/4, ROW/2-ROW/4, COL-(2*COL)/3, "Stdin");
-    ioFrame * output = new ioFrame((2*COL)/3, ROW/2, ROW-ROW/2, COL-(2*COL)/3, "Stdout");
+    ioFrame * frames[] = { 
+        new ioFrame(0, 0, ROW, (2*COL)/3, "Editor"),
+        new ioFrame((2*COL)/3, 0, ROW/4, COL-(2*COL)/3, "Arguments"),
+        new ioFrame((2*COL)/3, ROW/4, ROW/2-ROW/4, COL-(2*COL)/3, "Stdin"),
+        new ioFrame((2*COL)/3, ROW/2, ROW-ROW/2, COL-(2*COL)/3, "Stdout")
+    };
     toolbar * toolb = new toolbar("Terminal IDE");
     
-    code->focus();
+    int focussed = 0;
+
+    frames[focussed]->content = "Code\nEditor\n";
+    frames[focussed]->putstr(frames[focussed]->content);
+    frames[focussed]->focus();
 
     int ch;
     while((ch=getch())!=KEY_F(1)){
-        continue;
+        switch (ch){
+        case KEY_DOWN:
+            frames[focussed]->curdown();
+            break;
+        case KEY_UP:
+            frames[focussed]->curup();
+            break;
+        case KEY_LEFT:
+            frames[focussed]->curleft();
+            break;
+        case KEY_RIGHT:
+            frames[focussed]->curright();
+            break;
+        case 127:
+        case '\b':
+        case KEY_BACKSPACE:
+            frames[focussed]->untypechr();
+            break;
+        default:
+            frames[focussed]->typechr(ch);
+        }
     }
     
     endwin();
